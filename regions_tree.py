2536b8a59a9f2e14b8313f65b475a049607c9009
Sergej Orlov <wladimirych@gmail.com>
2022-03-30 09:32:19 +0200

    add support for use API key for server requests

--- regions_tree.py
+++ regions_tree.py
@@ -13,26 +13,26 @@ class RegionsTree(object):
         return cls(json.load(fd))
 
     @classmethod
-    def from_remote(cls, api_host=None):
+    def from_remote(cls, api_key, api_host=None):
         if api_host is None:
             api_host = cls.default_api_host
-        return cls(cls._download_tree(api_host))
+        return cls(cls._download_tree(api_key, api_host))
 
     def save_to_file(self, fd):
         json.dump(self.tree, fd)
 
     @classmethod
-    def _get_westra_region_data(cls, region_id, api_host):
-        url = '%s/passes/classificator.php?place=%s&export=json' % (api_host, region_id)
+    def _get_westra_region_data(cls, region_id, api_key, api_host):
+        url = '%s/passes/classificator.php?place=%s&export=json&key=%s' % (api_host, region_id, api_key)
         res = urllib2.urlopen(url, timeout=60)
         return json.load(res)
 
     @classmethod
-    def _download_tree(cls, api_host):
-        top_level_regions  = cls._get_westra_region_data('0', api_host)
+    def _download_tree(cls, api_key, api_host):
+        top_level_regions  = cls._get_westra_region_data('0', api_key, api_host)
         return {
             'id': '0',
-            'places': [cls._get_westra_region_data(region['id'], api_host) for region in top_level_regions],
+            'places': [cls._get_westra_region_data(region['id'], api_key, api_host) for region in top_level_regions],
             'title': 'World',
             'passes': []
         }
