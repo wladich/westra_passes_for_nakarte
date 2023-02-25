13bbabe562446ab0bd3978229d36d0569c877aba
Sergej Orlov <wladimirych@gmail.com>
2023-02-25 12:13:56 +0100

    Use api-key when writing source tree to json

--- tree_to_file_for_debugging.py
+++ tree_to_file_for_debugging.py
@@ -1,8 +1,20 @@
 from regions_tree import RegionsTree
 import sys
+from argparse import ArgumentParser
 
-[filename] = sys.argv[1:]
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('output_tree')
+    parser.add_argument('--api-host', default='https://westra.ru')
+    parser.add_argument('--api-key', required=True)
+    conf = parser.parse_args()
 
-regions = RegionsTree.from_remote()
-with open(filename, 'w') as f:
-    regions.save_to_file(f)
+    regions = RegionsTree.from_remote(conf.api_key)
+    with open(conf.output_tree, 'w') as f:
+        regions.save_to_file(f)
+
+
+if __name__ == '__main__':
+    main()
+
+
